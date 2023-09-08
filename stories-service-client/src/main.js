68d72d0305059a9d7e31674b95a7ba7a95a1b9fb
Gosha <kovalenko@giria.io>
2023-09-08 09:15:26 +0200

    Added global error handler

--- stories-service-client/src/main.js
+++ stories-service-client/src/main.js
@@ -11,6 +11,15 @@ app.use(router);
 axios.defaults.baseURL = 'http://localhost:3000/api';
 axios.defaults.headers.common.Authorization = "Bearer " + localStorage.getItem("authToken");
 
+axios.interceptors.response.use(undefined, function (error) {
+    if(error.response.status === 401) {
+        window.location.href = "/";
+
+        return;
+    }
+    return Promise.reject(error);
+})
+
 app.use(VueAxios, axios);
 
 app.mount('#app')
