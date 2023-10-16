ddf92b9eb274b027cd485b73217004948bbe3eee
Gosha Kovalenko <kovalenko@giria.io>
2023-10-16 11:32:18 +0200

    Added upload image to editor

--- stories-service-client/src/components/my-stories/publish-stories/PublishStories.vue
+++ stories-service-client/src/components/my-stories/publish-stories/PublishStories.vue
@@ -6,8 +6,15 @@
       <Editor
           api-key="fqqboid3jj5dviefd7qko96da2nnz3run5y6af5t891srayh"
           :init="{
-        plugins: 'lists link image table code help wordcount'
-      }"
+            plugins: 'lists link image table code help wordcount',
+            language: 'uk',
+            plugins: 'image code',
+            toolbar: 'image undo redo | blocks | bold italic | alignleft aligncentre alignright alignjustify | indent outdent | bullist numlist',
+            image_title: true,
+            automatic_uploads: true,
+            file_picker_types: 'image',
+            file_picker_callback: getImageInEditor
+        }"
           v-model="story"
       />
       <button class="publish-story-btn" type="submit">{{getTranslateByKeyLocal('system.ui.translate.publish_story').setting_value}}</button>
@@ -29,6 +36,35 @@
       }
     },
     methods: {
+      getImageInEditor(cb, value, meta) {
+          const input =  document.createElement('input');
+          input.setAttribute('type', 'file');
+          input.setAttribute('accept', 'image/*');
+
+          input.addEventListener('change', (e) => {
+            const file = e.target.files[0];
+
+            const reader = new FileReader();
+            reader.addEventListener('load', () => {
+              /*
+                Note: Now we need to register the blob in TinyMCEs image blob
+                registry. In the next release this part hopefully won't be
+                necessary, as we are looking to handle it internally.
+              */
+              const id = 'blobid' + (new Date()).getTime();
+              const blobCache =  tinymce.activeEditor.editorUpload.blobCache;
+              const base64 = reader.result.split(',')[1];
+              const blobInfo = blobCache.create(id, file, base64);
+              blobCache.add(blobInfo);
+
+              /* call the callback and populate the Title field with the file name */
+              cb(blobInfo.blobUri(), { title: file.name });
+            });
+            reader.readAsDataURL(file);
+          });
+
+          input.click();
+      },
       async saveStory(e) {
         e.preventDefault();
 
