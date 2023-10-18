10d50457ad79f1d7d3f6f9c1450cb6e5de605890
Gosha Kovalenko <kovalenko@giria.io>
2023-10-18 15:16:20 +0200

    Added not found story logo

--- stories-service-client/src/components/stories/story/Story.vue
+++ stories-service-client/src/components/stories/story/Story.vue
@@ -1,9 +1,16 @@
 <template>
-  <h1 class="story-title">{{storyTitle}}</h1>
-  <h3 class="story-theme">Theme: {{storyTheme}}</h3>
-  <div v-html="story" class="wrapper__story"></div>
+  <div v-if="story">
+    <h1 class="story-title">{{storyTitle}}</h1>
+    <h3 class="story-theme">Theme: {{storyTheme}}</h3>
+    <div v-html="story" class="wrapper__story"></div>
+  </div>
+  <div v-if="!story">
+    <h2 class="not-found-logo">{{getTranslateByKeyLocal("system.ui.translate.story_not_found")}}</h2>
+  </div>
 </template>
 <script>
+  import {getTranslateByKey} from "../../common/getTranslateByKey";
+
   export default {
     data() {
       return {
@@ -11,6 +18,11 @@
         storyTitle: "",
         storyTheme: ""
       }
+    },
+    methods: {
+      getTranslateByKeyLocal(key) {
+        return getTranslateByKey(key).setting_value;
+      },
     },
     async mounted() {
       this.story = (await this.axios.get("/stories/" + this.$route.params.hash)).data.story;
@@ -33,6 +45,9 @@
     margin-top: 50px;
   }
   .story-theme {
+    text-align: center;
+  }
+  .not-found-logo {
     text-align: center;
   }
 </style>
